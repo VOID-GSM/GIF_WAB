48d45258d3cd6241943fdacb135c30c377d11825
leehyoeun <s25049@gsm.hs.kr>
2026-05-29 14:33:32 +0900

    style: 앱 전역 폰트와 UI 소스 설정

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,4 +1,8 @@
+import "./globals.css";
+import { Righteous } from "next/font/google";
 import { Providers } from "./providers";
+
+const righteous = Righteous({ weight: "400", variable: "--font-righteous", subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -6,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={righteous.variable}>
       <body>
         <Providers>{children}</Providers>
       </body>
