1c4db05c52aff9b16edfe325d280e66f79513fb2
leehyoeun <s25049@gsm.hs.kr>
2026-05-29 15:04:58 +0900

    feat: 404 페이지에 메인 이동 옵션 추가

--- packages/ui/src/NotFound.tsx
+++ packages/ui/src/NotFound.tsx
@@ -7,18 +7,21 @@ export default function NotFound() {
 
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center gap-4">
-      <span className="text-8xl  leading-none tracking-tight text-[var(--color-yellow-600)] font-righteous">
+      <span className="text-8xl  leading-none tracking-tight text-[var(--color-yellow-600)] font-righteous mb-4">
         404 ERROR
       </span>
       <p className="text-2xl font-medium text-[var(--color-gray-700)]">
         죄송합니다. 페이지를 찾을 수 없습니다
       </p>
-      <button
-        onClick={() => router.back()}
-        className="underline text-[var(--color-orange-600)] bg-transparent border-none cursor-pointer"
-      >
-        이전 페이지로 이동하기
-      </button>
+      <div className="flex gap-4">
+        <button
+          onClick={() => router.back()}
+          className="cursor-pointer border-none bg-transparent text-[var(--color-orange-600)] underline"
+          type="button"
+        >
+          이전 페이지로 이동하기
+        </button>
+      </div>
     </div>
   );
 }
